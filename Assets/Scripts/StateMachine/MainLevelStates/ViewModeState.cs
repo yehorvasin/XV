5588bcbdbe3730697cc7d6be6eb86f5c72abf742
Yehor Vasin <yvasin@e1r9p1.unit.ua>
2019-04-10 23:25:28 +0300

    temp

--- Assets/Scripts/StateMachine/MainLevelStates/ViewModeState.cs
+++ Assets/Scripts/StateMachine/MainLevelStates/ViewModeState.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class ViewModeState : MonoBehaviour, IState
 {
+    private CinemachineVirtualCamera _overviewCamera;
+    private CinemachineVirtualCamera _firstPersonCamera;
+    private CinemachineVirtualCamera _freeLookCamera;
+
+    private void Start()
+    {
+        _overviewCamera = GameController.Instance.OverviewCamera;
+        _firstPersonCamera = GameController.Instance.FirstPersonCamera;
+        _freeLookCamera = GameController.Instance.FreeLookCamera;
+    }
+
     public void ActivateState()
     {
         
@@ -21,6 +33,7 @@ public class ViewModeState : MonoBehaviour, IState
 
     public void KeysInputAction()
     {
+        #region handleKeys
         if (Input.GetKey(KeyCode.W))
         {
         }
@@ -42,5 +55,27 @@ public class ViewModeState : MonoBehaviour, IState
         else if (Input.GetKey(KeyCode.Backspace))
         {
         }
+        #endregion
+    }
+
+    public void SwitchToOverview()
+    {
+        _overviewCamera.gameObject.SetActive(true);
+        _firstPersonCamera.gameObject.SetActive(false);
+        _freeLookCamera.gameObject.SetActive(false);
+    }
+
+    public void SwitchToFirstPerson()
+    {
+        _overviewCamera.gameObject.SetActive(false);
+        _firstPersonCamera.gameObject.SetActive(true);
+        _freeLookCamera.gameObject.SetActive(false);
+    }
+
+    public void SwitchToFreeLook()
+    {
+        _overviewCamera.gameObject.SetActive(false);
+        _firstPersonCamera.gameObject.SetActive(false);
+        _freeLookCamera.gameObject.SetActive(true);
     }
 }
