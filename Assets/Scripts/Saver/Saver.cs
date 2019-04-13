6db0adb0cfd7ac39d8eb24255d052f0178df3837
Yehor Vasin <yvasin@e1r10p3.unit.ua>
2019-04-14 01:03:24 +0300

    create test scene for loading assetbundles

--- Assets/Scripts/Saver/Saver.cs
+++ Assets/Scripts/Saver/Saver.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Policy;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Saver : MonoBehaviour
 {
