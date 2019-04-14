da11cd694353b691d174a05e0eb2d37c359f24cd
Yehor Vasin <yvasin@e1r9p3.unit.ua>
2019-04-14 19:02:41 +0300

    UI in menu for loading objects

---
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject[] MenuObjects;
+    public GameObject[] LoadUIObjects;
+
+    private void Start()
+    {
+        OpenMenuScenesUI();
+    }
+
+    public void OpenLoadNewObjectsUI()
+    {
+        for (var i = 0; i < MenuObjects.Length; i++)
+            MenuObjects[i].SetActive(false);
+        
+        for (var i = 0; i < LoadUIObjects.Length; i++)
+            LoadUIObjects[i].SetActive(true);
+    }
+
+    public void OpenMenuScenesUI()
+    {
+        for (var i = 0; i < MenuObjects.Length; i++)
+            MenuObjects[i].SetActive(true);
+        
+        for (var i = 0; i < LoadUIObjects.Length; i++)
+            LoadUIObjects[i].SetActive(false);
+    }
+}
