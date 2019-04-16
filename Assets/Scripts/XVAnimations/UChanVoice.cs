320ff39c23303eeb6b56c09300d030325bcfda0e
asoroka <alissoroka@gmail.com>
2019-04-16 23:06:56 +0300

    add anim to take and run/walk

---
+++ Assets/Scripts/XVAnimations/UChanVoice.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UChanVoice : MonoBehaviour
+{
+	public AudioClip breathing;
+	public AudioClip hehe;
+
+	AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
