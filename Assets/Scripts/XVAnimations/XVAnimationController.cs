279060f537919323b0df2a466674e21586fc996a
asoroka <alissoroka@gmail.com>
2019-04-21 17:22:51 +0300

    save seq playing

--- Assets/Scripts/XVAnimations/XVAnimationController.cs
+++ Assets/Scripts/XVAnimations/XVAnimationController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public delegate void AnimCallBack();
@@ -172,7 +173,7 @@ public class XVAnimationController : MonoBehaviour
     public void StartSeq()
     {
 
-        if (!isPlaying)
+        if (!isPlaying && stack.Count > 0)
         {
             isPlaying = true;
             DisplayToUser(stack[0].GetDescription());
